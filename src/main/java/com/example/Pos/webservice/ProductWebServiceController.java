7116a2280a2bec29701b06a9e8adf698c738cfc4
hieuvu99 <64103982+hieuvu99@users.noreply.github.com>
2024-03-14 02:24:50 -0400

    update cors

--- src/main/java/com/example/Pos/webservice/ProductWebServiceController.java
+++ src/main/java/com/example/Pos/webservice/ProductWebServiceController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,18 +23,19 @@ public class ProductWebServiceController {
 	public ProductWebServiceController(ProductService productService) {
 		this.productService = productService;
 	}
-
+	
+	@CrossOrigin(origins = "http://localhost:3000") // Replace with your Next.js development server URL
 	@RequestMapping(path = "/products", method = RequestMethod.GET)
 	public List<Product> getProduct(Model model) {
 		return this.productService.getAllProduct();
 	}
-
+	@CrossOrigin(origins = "*") // Allow requests from any origin
 	@RequestMapping(path = "/products", method = RequestMethod.POST)
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Product addProduct(@RequestBody Product product) {
 		return this.productService.addProduct(product);
 	}
-
+	
 	@RequestMapping(path = "/products", method = RequestMethod.PUT)
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Product updateProduct(@RequestBody Product product) {
